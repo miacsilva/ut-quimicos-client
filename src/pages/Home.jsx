fed21c4ddccea3c963d288adc245391d5e5f20cc
miacsilva <miacsilvac@hotmail.com>
2023-05-03 17:43:09 +0100

    nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <div className="homeContainer">
-        <h3>
+        <h3 className="alert alert-info">
           Criada em 2008, a U. T. Químicos é uma empresa de distribuição de
           produtos químicos e matérias primas para a indústria transformadora.
           <br />A nossa equipa experiente e dinâmica procura de forma contínua:
@@ -21,7 +21,7 @@ function Home() {
         </h3>
 
         <div className="cardsContainer1">
-          <div className="cardContainer2">
+          <div className="cardContainer2 alert alert-info" role="alert">
             <h2>- Privilegiar/personalizar serviços e necessidades dos clientes.</h2>
             <h2>-Fidelizar clientes e fornecedores.</h2>
             <h2>-Estabelecer permanente contacto e
