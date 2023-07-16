8416c6f19298cd7b1f3c89394a9720f25974bd54
miacsilva <miacsilvac@hotmail.com>
2023-07-16 23:59:04 +0100

    sobre page

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -3,13 +3,32 @@ function Sobre() {
   return (
 
     <div id="sobre">
+    <header>
+        <h2>Sobre</h2>
+      </header>
+      <section >
+      <h3>A EMPRESA</h3>
+        <p className="sobreTexto"> Fundados em 2009, destacamo-nos no mercado enquanto empresa de distribuição de produtos químicos e matérias-primas dirigidas à indústria. A nossa rede de parcerias internacional permite-nos representar em Portugal várias empresas que se espalham pela União Europeia e Ásia.  </p> 
+        
+        <p className="sobreTexto">
 
+No core da empresa centramo-nos em matérias-primas de base, ou seja, especialidades químicas que são fornecidas às diversas indústrias, para que estas produzam os produtos finais. Com escritórios em Braga, temos a nossa base de logística em Santo Tirso através de um sistema outsourcing de um armazém especializado em logística de produtos químicos, responsável pela receção, stockagem e envio de mercadorias para os nossos clientes. </p> 
+<p className="sobreTexto">
+
+Devido à exigência técnica do setor químico, na UTQ fazemos o acompanhamento contínuo às inovações da área de forma a propormos e oferecermos a cada cliente as soluções mais adequadas para a ótimização dos seus processos produtivos. </p>
+<p className="sobreTexto">
+
+Continuamos procurar crescer no setor enquanto empresa de referência, quer no profissionalismo como na qualidade. </p>
+      </section>
     
 
 
 
 
+    <button className="goSomewhereButton">PRODUTOS</button>
+    <button className="goSomewhereButton">CONTACTE-NOS</button>
     </div>
+
 
   )
 }
